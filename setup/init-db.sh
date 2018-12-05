4d8c602cd70bdde5408c08bc63dae4639130c196
Chloé Ichinose <chloedv99@gmail.com>
2018-12-05 02:34:31 +0100

    Added database init script

---
+++ setup/init-db.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+Prompt() {
+    printf '%s [%s]: ' "$2" "$3"
+    read value
+    value="${value:=$3}"
+    eval "$1='$value'"
+}
+
+PromptHidden() {
+    printf '%s: ' "$2"
+    read -s value
+    echo
+    value="${value:=}"
+    eval "$1='$value'"
+}
+
+echo '# This script will initialize the app database.'
+echo '# An account with admin privileges is required, but it will only be used for this script.'
+echo
+
+
+Prompt dbhost 'MySQL server host' 'localhost'
+
+Prompt dbuser 'Database username' 'kanbans'
+PromptHidden dbpass 'Database password'
+
+Prompt dbname 'Database name' 'kanbans'
+
+Prompt admuser 'MySQL admin username' 'root'
+PromptHidden admpass 'MySQL admin password'
+
+
+mysql -h "$dbhost" -u "$admuser" -p"$admpass" <<EOT
+
+CREATE DATABASE IF NOT EXISTS $dbname;
+
+CREATE USER IF NOT EXISTS $dbuser;
+
+GRANT SELECT, INSERT, DELETE, UPDATE
+    ON $dbname.* TO '$dbuser'@'$dbhost' IDENTIFIED BY '$dbpass';
+
+EOT
+
+if [ $? -eq 0 ]; then
+    echo
+    echo '# Database initialized successfully.'
+    echo '# Make sure to regenerate your config file accordingly.'
+else
+    echo
+    echo '# An error occured during the initialization.'
+    echo '# See error above.'
+fi
